498341d5bc19ba308b013cb2f0e83456f6cb9f18
Rishabh-07-coder-stack <rayrishabh814@gmail.com>
2026-09-24 15:15:42 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"enter the number :";
+cin>>num;
+if(num %2 == 0)
+{
+cout<<endl<<num<<"is even";
+
+}
+else
+ {
+cout <<endl<<num<<"is odd ";
+}
+return 0;
+}
