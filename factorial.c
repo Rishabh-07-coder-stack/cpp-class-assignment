4c267b1b41ac9ae394d4e037f5cc8f389dbc4355
Rishabh-07-coder-stack <rayrishabh814@gmail.com>
2026-09-25 07:06:14 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+long long factorial=1;
+ cout<<"enter a number :";
+ cin>>n;
+ for(int i=1; i<=n; i++)
+ {
+
+    factorial=factorial*i;
+ }
+cout<<"factorial="<<factorial;
+return 0;
+
+}
